f2bf364237d2e31641e15c44c1ec171dcff3d2a2
FelixxDP <mb20472@bristol.ac.uk>
2023-07-07 19:47:38 +0100

    fixed major bug re: video input order simplified out_path option paths now cross-platform

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from pathlib import Path
 from pdb import set_trace as bp
 
 import cv2
@@ -15,9 +16,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--space_scale', type=int, default=4, help="upsampling space scale")
 parser.add_argument('--time_scale', type=int, default=8, help="upsampling time scale")
 parser.add_argument('--data_path', type=str, required=True, help="data path for testing")
-parser.add_argument('--out_path_lr', type=str, default="./output/LR/", help="output path (Low res image)")
-parser.add_argument('--out_path_bicubic', type=str, default="./output/Bicubic/", help="output path (bicubic upsampling)")
-parser.add_argument('--out_path_ours', type=str, default="./output/VideoINR/", help="output path (VideoINR)")
+parser.add_argument('--out_path', type=str, default="./demo_output/", help="output path (with subdirs for BC, LR and VideoINR)")
+# parser.add_argument('--out_path_lr', type=str, default="./output/LR/", help="output path (Low res image)")
+# parser.add_argument('--out_path_bicubic', type=str, default="./output/Bicubic/", help="output path (bicubic upsampling)")
+# parser.add_argument('--out_path_ours', type=str, default="./output/VideoINR/", help="output path (VideoINR)")
 parser.add_argument('--model_path', type=str, default="latest_G.pth", help="model parameter path")
 opt = parser.parse_known_args()[0]
 
@@ -40,17 +42,24 @@ def single_forward(model, imgs_in, space_scale, time_scale):
         model_output = model(imgs_temp, time_Tensors, space_scale, test=True)
         return model_output
 
+path_LR = Path(opt.out_path).joinpath("LR")
+path_BC = Path(opt.out_path).joinpath("Bicubic")
+path_VideoINR = Path(opt.out_path).joinpath("VideoINR")
+os.makedirs(path_LR, exist_ok=True)
+os.makedirs(path_BC, exist_ok=True)
+os.makedirs(path_VideoINR, exist_ok=True)
 
-os.makedirs(opt.out_path_lr, exist_ok=True)
-os.makedirs(opt.out_path_bicubic, exist_ok=True)
-os.makedirs(opt.out_path_ours, exist_ok=True)
+# correctly sort input videos by number, should work for "001" and "1" formats
+path_list = list(Path(opt.data_path).iterdir())
+filestem_list = np.array([f.stem for f in path_list], int)
+sort_idx = filestem_list.argsort()
+path_list = np.array(list(path_list), str)[sort_idx]
 
-path_list = [os.path.join(opt.data_path, name) for name in sorted(os.listdir(opt.data_path))]
 index = 0
-for ind in tqdm(range(len(path_list) - 1)):
+for idx in tqdm(range(len(path_list) - 1)):
 
-    imgpath1 = os.path.join(path_list[ind])
-    imgpath2 = os.path.join(path_list[ind + 1])
+    imgpath1 = path_list[idx]
+    imgpath2 = path_list[idx + 1]
 
     img1 = cv2.imread(imgpath1, cv2.IMREAD_UNCHANGED)
     img2 = cv2.imread(imgpath2, cv2.IMREAD_UNCHANGED)
@@ -65,9 +74,9 @@ for ind in tqdm(range(len(path_list) - 1)):
     img1 = imresize_np(img1, 1 / opt.space_scale, True).astype(np.float32) / 255.
     img2 = imresize_np(img2, 1 / opt.space_scale, True).astype(np.float32) / 255.
 
-    # save LR image1
+    # save LR from img1
     imgLR = Image.fromarray((np.clip(img1[:, :, [2, 1, 0]], 0, 1) * 255).astype(np.uint8))
-    imgLR.save(os.path.join(opt.out_path_lr, path_list[ind].split('/')[-1]))
+    imgLR.save(path_LR.joinpath(os.path.basename(imgpath1)))
 
     # concat imgs
     imgs = np.stack([img1, img2], axis=0)[:, :, :, [2, 1, 0]]
@@ -78,12 +87,11 @@ for ind in tqdm(range(len(path_list) - 1)):
 
     # Save results of VideoINR and bicubic up-sampling.
     for out_ind in range(len(output)):
-
         img = output[out_ind][0]
         img = Image.fromarray((img.clamp(0., 1.).detach().cpu().permute(1, 2, 0) * 255).numpy().astype(np.uint8))
-        img.save(os.path.join(opt.out_path_ours, '{}.png'.format(index)))
+        img.save(path_VideoINR.joinpath("{}.png".format(index)))
 
         HH, WW = img1.shape[0] * 4, img1.shape[1] * 4
         imgBC = Image.fromarray((np.clip(img1[:, :, [2, 1, 0]], 0, 1) * 255).astype(np.uint8)).resize((WW, HH),Image.BICUBIC)
-        imgBC.save(os.path.join(opt.out_path_bicubic, '{}.png'.format(index)))
+        imgBC.save(path_BC.joinpath("{}.png".format(index)))
         index += 1
