321cc1495835830742836662b0e6f07f85a4cc6a
Felix Dubicki-Piper <mb20472@bc4gpulogin1.bc4.acrc.priv>
2023-07-26 17:37:04 +0100

    remove FR downsample

--- video_processing/adobe2degraded.sh
+++ video_processing/adobe2degraded.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-# Generate Low-Frame-Rate, noisy, Low-Res .png frames from adobe240 dataset.
+# Generate noisy, Low-Res .png frames from adobe240 dataset.
 
-trainlist=data/adobe240fps_folder_train.txt
-testlist=data/adobe240fps_folder_test.txt
-validlist=data/adobe240fps_folder_valid.txt
+trainlist=../data/adobe240fps_folder_train.txt
+testlist=../data/adobe240fps_folder_test.txt
+validlist=../data/adobe240fps_folder_valid.txt
 vidlist=("$trainlist" "$testlist" "$validlist")
 subdir=(train test valid)
 
-FR_scale=8
-max_frames_in=800 # gives 100 frames output
+FR_scale=1 # DO NOT reduce frame rate (important for VideoINR training)
+max_frames=100 # output frames 0-100
 
 echo Start time: "$(date)"
 
 for i in 0 1 2; do
     while IFS=$' \t\n\r' read -r vname; do
-        outdir="adobe240/framesLQ_800/${subdir[i]}/$vname" # for LFR+noisy
+        outdir="adobe240/framesLQ_${max_frames}/${subdir[i]}/${vname}" # for LFR+noisy
         if [ ! -e "$outdir" ]; then
             echo $'\n\n'"STARTED video $vname"$'\n'
             inpath=$(find adobe240/original_videos/"$vname".*) # video input
             tmpdir=$(mktemp -d -p .)                           # for LFR
 
             # make LFR, save to temp
-            ./video2frame.sh "$inpath" "$FR_scale" "$max_frames_in" "$tmpdir"
+            ./video2frame.sh "$inpath" "$FR_scale" "$max_frames" "$tmpdir"
 
             # make noisy from temp, save to out
             echo "Exporting frames to $outdir using MATLAB..."
